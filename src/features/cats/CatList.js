945039b8cb8f4a689795b444d3349fe0bab94b68
Breanne <BreMarotta@gmail.com>
2022-06-17 12:12:13 -0400

    all tests passing including bonus loading status

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,13 @@
-// write your CatList component here
+import React from 'react'
+
+const CatList = ({ catPics = [] }) => {
+ 
+  return (
+    <div>
+        {catPics.map(c => <img key={c.id} src={c.url} alt="cat" width="250"></img>)}
+    </div>
+    
+  )
+}
+
+export default CatList
--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux"; 
+import { fetchCats } from './catsSlice'
+import CatList from "./CatList"
 
 function Cats() {
+  const catPics = useSelector(state => state.entities);
+  const status = useSelector(state => state.status)
+  const dispatch = useDispatch()
+
+  const showStatus = status === "loading" ? "Loading..." : ""
+
+  useEffect(() => {
+    dispatch(fetchCats());
+  }, [dispatch])
+  
   return (
     <div>
       <h1>CatBook</h1>
-      {/* add CatList component here */}
+      {showStatus}
+      <CatList catPics={catPics}/>
     </div>
   );
 }
